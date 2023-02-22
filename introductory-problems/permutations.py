357fcdeb4e5121e72662b1a813166a6455ddbafe
Arnav Shah <shaharnav03@gmail.com>
2023-02-21 20:37:53 -0500

    brain > recursion

--- introductory-problems/permutations.py
+++ introductory-problems/permutations.py
@@ -1,29 +1,43 @@
 # TODO: experiment with dynamic approach to reduce time complexity
-n = int(input())
-permutation = []
-chosen = [False for _ in range(n+1)]
 
-def search():
-    if (len(permutation) == n):
-        f = False
-        for i in range(n-1):
-            if abs(permutation[i] - permutation[i+1]) == 1:
-                f = True
-                break
-        
-        if not f:
-            print(*permutation)
-            quit()
+"""
+in: 5
+
+out: 1, 3, 5, 2, 4
+
+
+in: 4
+out: 2, 4, 1, 3
+
+
+
+1. If n = 1, return 1
+
+2. if n ∈ {2, 3}, return no sol
+
+3. Until n letters are obtained:
+    - Start at 2, start incrementing two
+    - If end of array is reached (or n-1), restart index at 1
+
+"""
+
+def solve(n):
+    if n == 1: return [1]
     
-    else:
-        for i in range(1, n+1):
-            if chosen[i]: continue
-            chosen[i] = True
-            permutation.append(i)
-            search()
-            chosen[i] = False
-            permutation.pop()
+    if n == 2 or n == 3: return["NO SOLUTION"]
 
-search()
+    sequence = []
+    i = 2
 
-print("NO SOLUTION")
+    while (len(sequence) < n):
+        sequence.append(i)
+        if (i + 2 <= n):
+            i += 2
+        else:
+            i = 1
+    
+    return sequence
+
+
+n = int(input())
+print(*solve(n))
